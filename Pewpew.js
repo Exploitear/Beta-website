c624cb2be5bf121b6fc2284c81ec28fa75c04608
Exploitear <armaazad5@gmail.com>
2025-07-11 17:08:57 +0900

    Updated site with latest changes

---
+++ Pewpew.js
@@ -0,0 +1,6 @@
+//i dont know javascript yet, though i will use this file for DOM manipulation to refresh pages without actually reloading
+//and for functionality of the page, like for taking user-inputs (forms), dragging window-popups etc
+//no backend stuff for now, no node.js, just frontend stuff for the page
+//myb i will make a reloading screen before it accesses index.html, like a animated loading icon and then index.html
+//i might need JS for that, it will be like 'connecting to the luxuria archive, executing thema' where the thema
+//is the website ig.
